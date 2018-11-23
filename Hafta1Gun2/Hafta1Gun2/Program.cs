c68a19fe8ebd31e210565189c43161397739cb0d
nastala <sezginyagli@gmail.com>
2018-11-23 20:22:01 +0300

    ...

---
+++ Hafta1Gun2/Hafta1Gun2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta1Gun2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
