63c0745054400499b36ef5096014ff7b48c921e7
nastala <sezginyagli@gmail.com>
2018-11-28 11:11:54 +0300

    ...

---
+++ Sezgin/Hafta2Gun3/Hafta2Gun3Part3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta2Gun3Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hafta 2, Gün 3, Part 2 <-----> 28.11.2018
+
+            ////Replace
+            //string cumle = "Selam Nasılsın?";
+            //string yeniCumle = cumle.Replace("Selam Nasılsın?", "Slm, Nbr?");
+            //Console.WriteLine(yeniCumle);
+
+            ////Remove
+            //string cumle = "Selam Nasılsın?";
+            //string yeniCumle = cumle.Remove(0, 6);
+            //Console.WriteLine(yeniCumle);
+
+            ////Substring
+            //string cumle = "Selam Nasılsın?";
+            //string yeniCumle = cumle.Substring(0, 6);
+            //Console.WriteLine(yeniCumle);
+
+            ////Trim
+            //string cumle = "       Selam Nasılsın?      ";
+            //string yeniCumle = cumle.TrimStart();
+            //Console.WriteLine(yeniCumle);
+
+            ////Split && Length
+            //string cumle = "A, B, C, D, E";
+            //string[] yeniCumle = cumle.Split(',');
+            //for (int i = 0; yeniCumle.Length; i++)
+            //    Console.WriteLine(yeniCumle[i]);
+
+            ////To Upper
+            //string cumle = "Selam Nasılsın?";
+            //string yeniCumle = cumle.ToUpper();
+            //Console.WriteLine(yeniCumle);
+
+            ////To Lower
+            //string cumle = "Selam Nasılsın?";
+            //string yeniCumle = cumle.ToLower();
+            //Console.WriteLine(yeniCumle);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sezgin/Hafta2Gun3/Hafta2Gun3Part3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hafta2Gun3Part3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Hafta2Gun3Part3")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f885c2f-a032-4c40-a920-cb7fed5bc5f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
