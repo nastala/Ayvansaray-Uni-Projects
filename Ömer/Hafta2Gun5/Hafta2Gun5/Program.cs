f5d3c74b070dbc6c013889d2aad13ac346a9bf10
nastala <sezginyagli@gmail.com>
2018-12-03 09:02:01 +0300

    ...

---
+++ Ömer/Hafta2Gun5/Hafta2Gun5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta2Gun5
+{
+    class Program
+    {
+        //Hafta 2, Gün 5, Part 1 <-----> 30.11.2018
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input :");
+            var amount = int.Parse(Console.ReadLine());
+            var limit = Math.Pow(2, amount);
+            var count = 0;
+
+            void Combination(IList<bool> arr, int lastIndex)
+            {
+                foreach (var val in arr)
+                {
+                    Console.Write(val + " ");
+                }
+
+                Console.WriteLine();
+                arr[lastIndex] = !arr[lastIndex++];
+                if (lastIndex >= arr.Count) lastIndex = 0;
+                if (++count >= limit)
+                {
+                    return;
+                }
+                Combination(arr, lastIndex);
+            }
+            Combination(new bool[amount], 0);
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ömer/Hafta2Gun5/Hafta2Gun5Part2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta2Gun5Part2
+{
+    class Program
+    {
+        //Hafta 2, Gün 5, Part 2 <-----> 30.11.2018
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input :");
+            var amount = int.Parse(Console.ReadLine());
+            var vals = new bool[amount];
+            var limit = Math.Pow(2, amount);
+            var changeIndex = 0;
+            var count = 0;
+
+            for (int q = 0; q < limit; q++)
+            {
+                foreach (var val in vals)
+                {
+                    Console.Write(val + " ");
+                }
+
+                Console.WriteLine();
+                vals[changeIndex] = !vals[changeIndex++];
+                if (changeIndex >= amount) changeIndex = 0;
+                ++count;
+            }
+
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
