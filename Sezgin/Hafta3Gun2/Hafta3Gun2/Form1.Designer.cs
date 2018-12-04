433e6b96c5f90bf52276b69e86f6fbdf225f1844
nastala <sezginyagli@gmail.com>
2018-12-04 10:14:35 +0300

    ...

---
+++ Sezgin/Hafta3Gun2/Hafta3Gun2/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Hafta3Gun2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbBoomNumber = new System.Windows.Forms.TextBox();
+            this.lbBoomNumbers = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbBoomNumber
+            // 
+            this.tbBoomNumber.Location = new System.Drawing.Point(123, 8);
+            this.tbBoomNumber.Name = "tbBoomNumber";
+            this.tbBoomNumber.Size = new System.Drawing.Size(156, 20);
+            this.tbBoomNumber.TabIndex = 0;
+            // 
+            // lbBoomNumbers
+            // 
+            this.lbBoomNumbers.FormattingEnabled = true;
+            this.lbBoomNumbers.Location = new System.Drawing.Point(12, 39);
+            this.lbBoomNumbers.Name = "lbBoomNumbers";
+            this.lbBoomNumbers.Size = new System.Drawing.Size(267, 173);
+            this.lbBoomNumbers.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 218);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(59, 25);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Boom Number: ";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(77, 218);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(59, 25);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 247);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbBoomNumbers);
+            this.Controls.Add(this.tbBoomNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbBoomNumber;
+        private System.Windows.Forms.ListBox lbBoomNumbers;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Sezgin/Hafta3Gun2/Hafta3Gun2/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Hafta3Gun2
+{
+    public partial class Form1 : Form
+    {
+        //Hafta 3, Gün 2, Part 1 <-----> 04.12.2018
+        string itemText;
+        byte boomNumber;
+        byte count = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += new System.EventHandler(OnTimerEvent);
+        }
+
+        private bool timerCheck()
+        {
+            if (timer1.Enabled)
+            {
+                tbBoomNumber.Clear();
+                MessageBox.Show("Timer is already running right now!");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!timerCheck())
+                return;
+
+            lbBoomNumbers.Items.Clear();
+
+            try
+            {
+                boomNumber = byte.Parse(tbBoomNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("You must enter a number");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("The number is too big.");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("You can not enter 0.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unknown error occured! Error: " + ex.Message);
+            }
+            finally
+            {
+                tbBoomNumber.Clear();
+            }
+            
+            //timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void OnTimerEvent(object source, EventArgs e)
+        {
+            if (count > 100)
+            {
+                timer1.Stop();
+                count = 1;
+                return;
+            }
+
+            if (count % boomNumber == 0)
+            {
+                lbBoomNumbers.BackColor = Color.Red;
+                lbBoomNumbers.ForeColor = Color.White;
+                itemText = "BOOM";
+            }
+            else
+            {
+                lbBoomNumbers.BackColor = Color.White;
+                lbBoomNumbers.ForeColor = Color.Black;
+                itemText = count.ToString();
+            }
+
+            lbBoomNumbers.Items.Add(itemText);
+            lbBoomNumbers.TopIndex = count - 1;
+            count++;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (!timerCheck())
+                return;
+
+            lbBoomNumbers.BackColor = Color.White;
+            lbBoomNumbers.Items.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //lbBoomNumbers.Items.Add(itemText);
+        }
+    }
+}
