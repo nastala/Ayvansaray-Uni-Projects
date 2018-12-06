ab4ebda2aabcd89eb1c5fb11a6d444ceff91a21f
nastala <sezginyagli@gmail.com>
2018-12-06 14:05:38 +0300

    ...

---
+++ Sezgin/Hafta3/Hafta3Gun4/Hafta3Gun4Part4/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Hafta3Gun4Part4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnChangeColor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 251);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 269);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Add Button";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnChangeColor
+            // 
+            this.btnChangeColor.Location = new System.Drawing.Point(93, 269);
+            this.btnChangeColor.Name = "btnChangeColor";
+            this.btnChangeColor.Size = new System.Drawing.Size(86, 23);
+            this.btnChangeColor.TabIndex = 2;
+            this.btnChangeColor.Text = "Change Color";
+            this.btnChangeColor.UseVisualStyleBackColor = true;
+            this.btnChangeColor.Click += new System.EventHandler(this.btnChangeColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 304);
+            this.Controls.Add(this.btnChangeColor);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnChangeColor;
+    }
+}
+
---
+++ Sezgin/Hafta3/Hafta3Gun4/Hafta3Gun4Part4/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta3Gun4Part4
+{
+    public partial class Form1 : Form
+    {
+        //Hafta 3, Gün 4, Part 4 <-----> 06.12.2018
+
+        private int buttonCounter = 1;
+        private List<Button> buttons = new List<Button>();
+        private int y = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int x;
+            if (buttonCounter % 2 == 0)
+                x = 87;
+            else
+            {
+                y = 42 * (buttonCounter / 2);
+                x = 0;
+            }
+
+            if (y > panel1.Height)
+            {
+                MessageBox.Show("You can not add a button any more!");
+                return;
+            }
+
+            Button button = new Button();
+            button.Parent = panel1;
+            button.Location = new Point(x, y);
+            button.Height = 40;
+            button.Width = 80;
+            button.Text = "Button " + buttonCounter;
+            button.Click += (bS, bE) =>
+            {
+                MessageBox.Show("You cliked on " + ((Button)bS).Text);
+            };
+            buttons.Add(button);
+            panel1.Controls.AddRange(buttons.ToArray());
+            buttonCounter++;
+        }
+
+        private void btnChangeColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            DialogResult result = colorDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                foreach (Button button in buttons)
+                    button.BackColor = colorDialog.Color;
+            }
+        }
+    }
+}
