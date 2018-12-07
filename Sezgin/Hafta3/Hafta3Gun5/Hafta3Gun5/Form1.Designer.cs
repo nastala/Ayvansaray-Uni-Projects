80e720b349e448e39a90439374facb82bb62e37b
nastala <sezginyagli@gmail.com>
2018-12-07 09:08:18 +0300

    ...

---
+++ Sezgin/Hafta3/Hafta3Gun5/Hafta3Gun5/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Hafta3Gun5
+{
+    partial class FormCatchMe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCatchMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCatchMe
+            // 
+            this.btnCatchMe.Location = new System.Drawing.Point(210, 235);
+            this.btnCatchMe.Name = "btnCatchMe";
+            this.btnCatchMe.Size = new System.Drawing.Size(80, 30);
+            this.btnCatchMe.TabIndex = 0;
+            this.btnCatchMe.Text = "Catch Me";
+            this.btnCatchMe.UseVisualStyleBackColor = true;
+            this.btnCatchMe.Click += new System.EventHandler(this.btnCatchMe_Click);
+            this.btnCatchMe.MouseEnter += new System.EventHandler(this.btnCatchMe_MouseEnter);
+            // 
+            // FormCatchMe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.btnCatchMe);
+            this.Name = "FormCatchMe";
+            this.Text = "Catch Me Game";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FormCatchMe_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCatchMe;
+    }
+}
+
---
+++ Sezgin/Hafta3/Hafta3Gun5/Hafta3Gun5/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta3Gun5
+{
+    public partial class FormCatchMe : Form
+    {
+        private Random r = new Random();
+        private int iAttemps = 0;
+        private int x, y;
+
+        public FormCatchMe()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCatchMe_MouseEnter(object sender, EventArgs e)
+        {
+            x = r.Next(0, this.Size.Width - btnCatchMe.Width);
+            y = r.Next(0, this.Size.Height - btnCatchMe.Height);
+            if (new Point(x, y) == FormCatchMe.MousePosition)
+                btnCatchMe_MouseEnter(sender, e);
+            
+            btnCatchMe.Location = new Point(x, y);
+        }
+
+        private void FormCatchMe_MouseClick(object sender, MouseEventArgs e)
+        {
+            iAttemps++;
+            this.Text = "Total Attemps = " + iAttemps;
+        }
+
+        private void btnCatchMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congrats. You're very lucky!");
+            this.Text = "Catch Me Game";
+            iAttemps = 0;
+        }
+    }
+}
