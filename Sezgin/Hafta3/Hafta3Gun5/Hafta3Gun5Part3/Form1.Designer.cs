b1627b3dfe712feb14677bc2ba4a6337f64b4c2a
nastala <sezginyagli@gmail.com>
2018-12-07 14:18:32 +0300

    ...

---
+++ Sezgin/Hafta3/Hafta3Gun5/Hafta3Gun5Part3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Hafta3Gun5Part3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.mtbTime = new System.Windows.Forms.MaskedTextBox();
+            this.btnSaveStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(12, 30);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDate.TabIndex = 0;
+            // 
+            // mtbTime
+            // 
+            this.mtbTime.Location = new System.Drawing.Point(12, 56);
+            this.mtbTime.Mask = "00:00";
+            this.mtbTime.Name = "mtbTime";
+            this.mtbTime.Size = new System.Drawing.Size(100, 20);
+            this.mtbTime.TabIndex = 1;
+            this.mtbTime.ValidatingType = typeof(System.DateTime);
+            // 
+            // btnSaveStart
+            // 
+            this.btnSaveStart.Location = new System.Drawing.Point(118, 56);
+            this.btnSaveStart.Name = "btnSaveStart";
+            this.btnSaveStart.Size = new System.Drawing.Size(94, 23);
+            this.btnSaveStart.TabIndex = 2;
+            this.btnSaveStart.Text = "Save and Start";
+            this.btnSaveStart.UseVisualStyleBackColor = true;
+            this.btnSaveStart.Click += new System.EventHandler(this.btnSaveStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 125);
+            this.Controls.Add(this.btnSaveStart);
+            this.Controls.Add(this.mtbTime);
+            this.Controls.Add(this.dtpDate);
+            this.Name = "Form1";
+            this.Text = "Alarm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.MaskedTextBox mtbTime;
+        private System.Windows.Forms.Button btnSaveStart;
+    }
+}
+
---
+++ Sezgin/Hafta3/Hafta3Gun5/Hafta3Gun5Part3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta3Gun5Part3
+{
+    public partial class Form1 : Form
+    {
+        //Hafta 3, Gün 5, Part 3 <-----> 07.12.2018
+        private DateTime selectedDate, startDate;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initializeVars();
+        }
+
+        private void initializeVars()
+        {
+            this.Text = DateTime.Now.ToString();
+            timer1.Interval = 1000;
+            startDate = DateTime.Now;
+            dtpDate.MinDate = startDate;
+            mtbTime.Text = startDate.Hour + startDate.Minute + "";
+        }
+
+        private void btnSaveStart_Click(object sender, EventArgs e)
+        {
+            selectedDate = dtpDate.Value;
+        }
+    }
+}
