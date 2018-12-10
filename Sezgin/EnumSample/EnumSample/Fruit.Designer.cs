d17616e61a62764730f6b933b58f12c18abaa3b1
nastala <sezginyagli@gmail.com>
2018-12-10 16:43:17 +0300

    Enum

---
+++ Sezgin/EnumSample/EnumSample/Fruit.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace EnumSample
+{
+    partial class Fruit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpFruits = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flpFruits
+            // 
+            this.flpFruits.Location = new System.Drawing.Point(12, 12);
+            this.flpFruits.Name = "flpFruits";
+            this.flpFruits.Size = new System.Drawing.Size(465, 342);
+            this.flpFruits.TabIndex = 0;
+            // 
+            // Fruit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 373);
+            this.Controls.Add(this.flpFruits);
+            this.Name = "Fruit";
+            this.Text = "Fruit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpFruits;
+    }
+}
+
---
+++ Sezgin/EnumSample/EnumSample/Fruit.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumSample
+{
+    public partial class Fruit : Form
+    {
+        private List<CustomClass> fruits;
+
+        public Fruit()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fruits = new List<CustomClass>();
+            fruits.Add(new CustomClass("Apple", CustomClass.Fruit.Apple));
+            fruits.Add(new CustomClass("Apricot", CustomClass.Fruit.Apricot));
+            fruits.Add(new CustomClass("Blackberry", CustomClass.Fruit.Blackberry));
+            fruits.Add(new CustomClass("Peach", CustomClass.Fruit.Peach));
+            fruits.Add(new CustomClass("Banana", CustomClass.Fruit.Banana));
+            fruits.Add(new CustomClass("Orange", CustomClass.Fruit.Orange));
+
+            string imagePath = @"D:\Ayvansaray Uni Projects\Ayvansaray-Uni-Projects\Sezgin\EnumSample\Images\";
+            string extension;
+            foreach(CustomClass fruit in fruits)
+            {
+                extension = "";
+
+                switch (fruit.Image)
+                {
+                    case CustomClass.Fruit.Apple:
+                        extension += "apple";
+                        break;
+                    case CustomClass.Fruit.Apricot:
+                        extension += "apricot";
+                        break;
+                    case CustomClass.Fruit.Blackberry:
+                        extension += "blackberry";
+                        break;
+                    case CustomClass.Fruit.Banana:
+                        extension += "banana";
+                        break;
+                    case CustomClass.Fruit.Peach:
+                        extension += "peach";
+                        break;
+                    case CustomClass.Fruit.Orange:
+                        extension += "orange";
+                        break;
+                }
+                extension += ".png";
+
+                flpFruitsCreateControl(fruit.ImageName, imagePath + extension);
+            }
+        }
+
+        private void flpFruitsCreateControl(string imageName, string imagePath)
+        {
+            Panel panel = new Panel();
+            panel.Parent = flpFruits;
+            panel.Size = new Size(110, 140);
+
+            Label lblImageName = new Label();
+            lblImageName.Size = new Size(60, 20);
+            lblImageName.Location = new Point(30, 5);
+            lblImageName.TextAlign = ContentAlignment.MiddleCenter;
+            lblImageName.Text = imageName;
+
+            PictureBox pbImage = new PictureBox();
+            pbImage.Size = new Size(100, 100);
+            pbImage.Location = new Point(5, 30);
+            pbImage.ImageLocation = imagePath;
+
+            panel.Controls.Add(lblImageName);
+            panel.Controls.Add(pbImage);
+            flpFruits.Controls.Add(panel);
+        }
+    }
+
+    public class CustomClass
+    {
+        public enum Fruit
+        {
+            Apple,
+            Blackberry,
+            Banana,
+            Peach,
+            Apricot,
+            Orange
+        }
+
+        public Fruit Image { get; }
+        public string ImageName { get; }
+
+        public CustomClass(string ImageName, Fruit Image)
+        {
+            this.Image = Image;
+            this.ImageName = ImageName;
+        }
+    }
+}
