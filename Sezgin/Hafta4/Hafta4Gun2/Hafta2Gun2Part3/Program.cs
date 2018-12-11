d0099d6673cee15d025ecfd5eacce51329b175d9
nastala <sezginyagli@gmail.com>
2018-12-11 14:17:50 +0300

    ...

---
+++ Sezgin/Hafta4/Hafta4Gun2/Hafta2Gun2Part3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hafta2Gun2Part3
+{
+    class Program
+    {
+        //Hafta 4, Gün 2, Part 3 <-----> 11.12.2018
+
+        static void Main(string[] args)
+        {
+            FileInfo NewFile = new FileInfo(@"C:\Test\HedefDizini\NewFile.txt");
+            if (NewFile.Exists) // File sınıfı: if(File.Exists(@"C:\Test\NewFile.txt"));
+            {
+                // Dosya silme işlemi
+                NewFile.Delete();
+            }
+
+            // Dosya oluşturma
+            NewFile.CreateText();
+            Console.WriteLine("Oluşturma Tarihi: " + NewFile.CreationTime);
+            Console.WriteLine("Son Erişim Tarihi: " + NewFile.LastAccessTime);
+            Console.WriteLine("Son Değiştirilme Tarihi: " + NewFile.LastWriteTime);
+            Console.WriteLine("Bulunduğu Dizinin Adı: " + NewFile.DirectoryName);
+            Console.WriteLine("Dısyanın Adı: " + NewFile.Name);
+            Console.WriteLine("Dosyanın Tam Adı: " + NewFile.FullName);
+            Console.WriteLine("Dosyanın Uzantısı: " + NewFile.Extension);
+            
+            if (File.Exists(@"C:\Test\TestDizini\" + NewFile.Name))
+                File.Delete(@"C:\Test\TestDizini\" + NewFile.Name);
+
+            // Dosya kopyalama
+            NewFile.CopyTo(@"C:\Test\TestDizini\" + NewFile.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sezgin/Hafta4/Hafta4Gun2/Hafta4Gun2Part2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Hafta4Gun2Part2
+{
+    class Program
+    {
+        //Hafta 4, Gün 2, Part 2, Directory Info <-----> 11.12.2018
+
+        static void Main(string[] args)
+        {
+            // Kullanılacak dizini tanımlama
+            DirectoryInfo NewDir = new DirectoryInfo(@"C:\Test\TestDizini");
+            try
+            {
+                // Dizinin var olup olmadığının kontrolü
+                if (NewDir.Exists)
+                {
+                    Console.WriteLine("Dizin mevcut.");
+                    // Dizin silme işlemi
+                    NewDir.Delete(true);
+                    Console.WriteLine("Dizin Silindi.");
+                }
+
+                // Yeni dizin oluşturma
+                NewDir.Create();
+                NewDir.Refresh();
+                Console.WriteLine("Klasör oluşturuldu.");
+                Console.WriteLine("Oluşturulma Tarihi: " + NewDir.CreationTime);
+                Console.WriteLine("Bulunduğu Dizinin Adı: " + NewDir.Parent);
+                Console.WriteLine("Dizinin Adı: " + NewDir.Name);
+                Console.WriteLine("Dizinin Tam Adı: " + NewDir.FullName);
+                Console.WriteLine("Son Erişim Tarihi: " + NewDir.LastAccessTime);
+                Console.WriteLine("Son Değiştirilme Tarihi: " + NewDir.LastWriteTime);
+                Console.ReadLine();
+
+                // Diziniçerisinde ait dizin oluşturma
+                DirectoryInfo SubDir = NewDir.CreateSubdirectory("AltDizin");
+                // Alt dizin içerisinde ait dizini oluşturma
+                SubDir.CreateSubdirectory("AltDizin2");
+                // GetFiles ile dizindeki dosyaların seçimi
+                Console.WriteLine("{0} dizinindeki dosya sayısı: {1}", NewDir.Name, NewDir.GetFiles().Length);
+                // GetDirectory ile dizindeki klasörlerin seçimi
+                Console.WriteLine("{0} dizinindeki klasör sayısı: {1}", NewDir.FullName, NewDir.GetDirectories().Length);
+
+                Console.ReadLine();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Hata oluştu. Hata mesajı: " + exc.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
