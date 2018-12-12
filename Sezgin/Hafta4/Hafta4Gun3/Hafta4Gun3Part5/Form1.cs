d28a4ff78785296813c48385d432a88e81fadd15
nastala <sezginyagli@gmail.com>
2018-12-12 14:17:02 +0300

    ...

--- Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part5/Form1.cs
+++ Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part5/Form1.cs
@@ -20,8 +20,44 @@ namespace Hafta4Gun3Part5
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            updateCbDrives();
+            updateTvDirectories();
+        }
+
+        private void updateTvDirectories()
+        {
+            tvDirectories.Nodes.Add(findDirectories(@"C:\", new TreeNode("")));
+        }
+
+        private TreeNode findDirectories(string path, TreeNode parentNode)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            if(directoryInfo.GetDirectories().Length > 0)
+            {
+                TreeNode subNode;
+                foreach (DirectoryInfo subDir in directoryInfo.GetDirectories())
+                {
+                    subNode = new TreeNode(subDir.Name);
+                    if (subDir.GetDirectories().Length > 0)
+                        return subNode.Nodes.Add(findDirectories(subDir.FullName, subNode));
+                    parentNode.Nodes.Add(subNode);
+                }
+            }
+            //else
+            //{
+            //    parentNode.Nodes.Add(treeNode);
+            //    return parentNode;
+            //}
+            return parentNode;
+        }
+
+        private void updateCbDrives()
+        {
             foreach (string drive in Directory.GetLogicalDrives())
                 cbDrives.Items.Add(drive);
+
+            if (cbDrives.Items.Count > 0)
+                cbDrives.SelectedIndex = 0;
         }
     }
 }
