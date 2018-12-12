def7c5103caa1d6a367ad60765cc05e27b540819
nastala <sezginyagli@gmail.com>
2018-12-12 11:03:30 +0300

    TreeView

---
+++ Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part3/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Hafta4Gun3Part3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] drives = Directory.GetLogicalDrives();
+            foreach (string drive in drives)
+            {
+                string[] subDirs = Directory.GetDirectories(drive);
+                TreeNode subNode;
+                List<TreeNode> treeNodes = new List<TreeNode>();
+                foreach (string subDir in subDirs)
+                {
+                    subNode = new TreeNode(subDir);
+                    treeNodes.Add(subNode);
+                }
+
+                tvDirectories.Nodes.Add(new TreeNode(drive, treeNodes.ToArray()));
+            }
+        }
+    }
+}
