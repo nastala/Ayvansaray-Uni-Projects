16a29d6d7067657d2f46df49b313863022553494
nastala <sezginyagli@gmail.com>
2018-12-12 10:14:26 +0300

    ...

--- Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part2/Program.cs
+++ Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Threading;
 
 namespace Hafta4Gun3Part2
 {
@@ -13,7 +14,10 @@ namespace Hafta4Gun3Part2
         {
             KareleriSeveriz kareler = new KareleriSeveriz();
             foreach (double kare in kareler)
+            {
                 Console.WriteLine(kare);
+                Thread.Sleep(100);
+            }
         }
     }
 
