696cec6ad06ae61c02c6e38689b646e2bab2070d
nastala <sezginyagli@gmail.com>
2018-12-12 09:48:46 +0300

    Kare IEnum

---
+++ Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Hafta4Gun3Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KareleriSeveriz kareler = new KareleriSeveriz();
+            foreach (double kare in kareler)
+                Console.WriteLine(kare);
+        }
+    }
+
+    class KareleriSeveriz : IEnumerable
+    {
+        private KareleriSeverizIEnumerator iter;
+
+        public KareleriSeveriz()
+        {
+            iter = new KareleriSeverizIEnumerator();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return iter;
+        }
+    }
+
+    class KareleriSeverizIEnumerator : IEnumerator
+    {
+        private int currentNumber;
+        private double kare;
+
+        public KareleriSeverizIEnumerator()
+        {
+            Reset();
+        }
+
+        public object Current { get { return kare; } }
+
+        public bool MoveNext()
+        {
+            kare = Math.Pow(currentNumber, 2);
+            currentNumber++;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            currentNumber = 0;
+            kare = 0;
+        }
+    }
+}
