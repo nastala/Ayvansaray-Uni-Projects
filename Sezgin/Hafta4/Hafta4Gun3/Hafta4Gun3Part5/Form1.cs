7ceb882521fb14061fea75dd4380da9e710224ac
nastala <sezginyagli@gmail.com>
2018-12-12 17:42:11 +0300

    ...

--- Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part5/Form1.cs
+++ Sezgin/Hafta4/Hafta4Gun3/Hafta4Gun3Part5/Form1.cs
@@ -13,6 +13,10 @@ namespace Hafta4Gun3Part5
 {
     public partial class Form1 : Form
     {
+        private const int oneKB = 1024;
+        private const int oneMB = oneKB * 1024;
+        private List<string> extensions;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,35 +24,44 @@ namespace Hafta4Gun3Part5
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            extensions = new List<string>();
             updateCbDrives();
-            updateTvDirectories();
+            initializeLvFiles();
         }
 
-        private void updateTvDirectories()
+        private void initializeLvFiles()
         {
-            tvDirectories.Nodes.Add(findDirectories(@"C:\", new TreeNode("")));
+            lvFiles.View = View.Details;
+            lvFiles.Columns.Add("Dosya Adı", 210, HorizontalAlignment.Left);
+            lvFiles.Columns.Add("Boyut", 50, HorizontalAlignment.Left);
+            lvFiles.Columns.Add("Oluşturma Tarihi", 240, HorizontalAlignment.Left);
         }
 
-        private TreeNode findDirectories(string path, TreeNode parentNode)
+        private void updateTvDirectories(string drive)
         {
-            DirectoryInfo directoryInfo = new DirectoryInfo(path);
-            if(directoryInfo.GetDirectories().Length > 0)
+            tvDirectories.Nodes.Add(findDirectories(@"D:\Ayvansaray Uni Projects", @"D:\Ayvansaray Uni Projects"));
+        }
+
+        private TreeNode findDirectories(string name, string path)
+        {
+            TreeNode currentNode = new TreeNode(name);
+            DirectoryInfo currentDirectory = new DirectoryInfo(path);
+            try
             {
-                TreeNode subNode;
-                foreach (DirectoryInfo subDir in directoryInfo.GetDirectories())
+                if (currentDirectory.GetDirectories().Length > 0)
                 {
-                    subNode = new TreeNode(subDir.Name);
-                    if (subDir.GetDirectories().Length > 0)
-                        return subNode.Nodes.Add(findDirectories(subDir.FullName, subNode));
-                    parentNode.Nodes.Add(subNode);
+                    foreach (DirectoryInfo directory in currentDirectory.GetDirectories())
+                    {
+                        currentNode.Nodes.Add(findDirectories(directory.Name, directory.FullName));
+                    }
                 }
             }
-            //else
-            //{
-            //    parentNode.Nodes.Add(treeNode);
-            //    return parentNode;
-            //}
-            return parentNode;
+            catch(Exception exc)
+            {
+
+            }
+
+            return currentNode;
         }
 
         private void updateCbDrives()
@@ -58,6 +71,59 @@ namespace Hafta4Gun3Part5
 
             if (cbDrives.Items.Count > 0)
                 cbDrives.SelectedIndex = 0;
+        }
+
+        private void cbDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(cbDrives.SelectedItem.ToString()))
+                updateTvDirectories(cbDrives.SelectedItem.ToString());
+        }
+
+        private void addToExtensions(string newExtension)
+        {
+            if (string.IsNullOrWhiteSpace(newExtension))
+                return;
+
+            foreach(string extension in extensions)
+            {
+                if (extension.Equals(newExtension))
+                    return;
+            }
+
+            extensions.Add(newExtension);
+        }
+
+        private void tvDirectories_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                lvFiles.Items.Clear();
+                extensions.Clear();
+                MessageBox.Show(e.Node.Text);
+                DirectoryInfo currentDirectory = new DirectoryInfo(e.Node.FullPath);
+                float totalSize = 0;
+                foreach (FileInfo file in currentDirectory.GetFiles())
+                {
+                    addToExtensions(file.Extension);
+                    lvFiles.Items.Add(new ListViewItem(new string[] { file.Name, file.Length.ToString(),
+                        String.Format("{0:dddd, MMMM d, yyyy}", file.CreationTimeUtc) }));
+                    totalSize += file.Length;
+                }
+
+                string text = "";
+                int numFiles = currentDirectory.GetFiles().Length;
+                if (numFiles > 1)
+                    text = "There are " + numFiles + " files. Size: ";
+                if (totalSize >= oneMB)
+                    text += string.Format("{0:0.00} MB", totalSize / oneMB);
+                else if (totalSize >= oneKB)
+                    text += string.Format("{0:0.00} KB", totalSize / oneKB);
+                else
+                    text += totalSize + " Bytes";
+
+                cbExtensions.Items.AddRange(extensions.ToArray());
+                lblStatus.Text = text;
+            }
         }
     }
 }
