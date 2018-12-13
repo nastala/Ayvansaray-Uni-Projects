698a108f6b8887aa15b31dfce1157fdbbdb957d5
nastala <sezginyagli@gmail.com>
2018-12-13 14:36:57 +0300

    Itına Ile Zar Atılır

---
+++ Sezgin/Hafta4/Hafta4Gun4/Hafta4Gun4/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Hafta4Gun4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ilDiceNumbers = new System.Windows.Forms.ImageList(this.components);
+            this.pbDiceLeft = new System.Windows.Forms.PictureBox();
+            this.pbDiceRight = new System.Windows.Forms.PictureBox();
+            this.btnVibrate = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ilDiceNumbers
+            // 
+            this.ilDiceNumbers.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilDiceNumbers.ImageStream")));
+            this.ilDiceNumbers.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilDiceNumbers.Images.SetKeyName(0, "dice1.jpg");
+            this.ilDiceNumbers.Images.SetKeyName(1, "dice2.jpg");
+            this.ilDiceNumbers.Images.SetKeyName(2, "dice3.jpg");
+            this.ilDiceNumbers.Images.SetKeyName(3, "dice4.jpg");
+            this.ilDiceNumbers.Images.SetKeyName(4, "dice5.jpg");
+            this.ilDiceNumbers.Images.SetKeyName(5, "dice6.jpg");
+            // 
+            // pbDiceLeft
+            // 
+            this.pbDiceLeft.Location = new System.Drawing.Point(13, 13);
+            this.pbDiceLeft.Name = "pbDiceLeft";
+            this.pbDiceLeft.Size = new System.Drawing.Size(100, 100);
+            this.pbDiceLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDiceLeft.TabIndex = 0;
+            this.pbDiceLeft.TabStop = false;
+            // 
+            // pbDiceRight
+            // 
+            this.pbDiceRight.Location = new System.Drawing.Point(156, 13);
+            this.pbDiceRight.Name = "pbDiceRight";
+            this.pbDiceRight.Size = new System.Drawing.Size(100, 100);
+            this.pbDiceRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDiceRight.TabIndex = 1;
+            this.pbDiceRight.TabStop = false;
+            // 
+            // btnVibrate
+            // 
+            this.btnVibrate.Location = new System.Drawing.Point(83, 120);
+            this.btnVibrate.Name = "btnVibrate";
+            this.btnVibrate.Size = new System.Drawing.Size(102, 35);
+            this.btnVibrate.TabIndex = 2;
+            this.btnVibrate.Text = "SALLA";
+            this.btnVibrate.UseVisualStyleBackColor = true;
+            this.btnVibrate.Click += new System.EventHandler(this.btnVibrate_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 167);
+            this.Controls.Add(this.btnVibrate);
+            this.Controls.Add(this.pbDiceRight);
+            this.Controls.Add(this.pbDiceLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDiceRight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList ilDiceNumbers;
+        private System.Windows.Forms.PictureBox pbDiceLeft;
+        private System.Windows.Forms.PictureBox pbDiceRight;
+        private System.Windows.Forms.Button btnVibrate;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Sezgin/Hafta4/Hafta4Gun4/Hafta4Gun4/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta4Gun4
+{
+    public partial class Form1 : Form
+    {
+        private int totalTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVibrate_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            totalTime = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            totalTime += timer1.Interval;
+            if(totalTime >= 2000)
+            {
+                timer1.Stop();
+                totalTime = 0;
+                btnVibrate.Location = new Point(80, 120);
+                Random r = new Random();
+                pbDiceLeft.Image = ilDiceNumbers.Images[r.Next(1, 6)];
+                pbDiceRight.Image = ilDiceNumbers.Images[r.Next(1, 6)];
+            }
+            else
+            {
+                if ((totalTime / timer1.Interval) % 2 == 0)
+                    btnVibrate.Location = new Point(60, 120);
+                else
+                    btnVibrate.Location = new Point(100, 120);
+            }
+        }
+    }
+}
