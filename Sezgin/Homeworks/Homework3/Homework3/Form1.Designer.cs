696c28b05d8dbab034a72f4ab8dffe8bbd1819d0
nastala <sezginyagli@gmail.com>
2018-12-16 15:02:50 +0300

    ...

---
+++ Sezgin/Homeworks/Homework3/Homework3/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Homework3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ilCharacter = new System.Windows.Forms.ImageList(this.components);
+            this.pbCharacter = new System.Windows.Forms.PictureBox();
+            this.timerCharacter = new System.Windows.Forms.Timer(this.components);
+            this.pbSource = new System.Windows.Forms.PictureBox();
+            this.pbTarget = new System.Windows.Forms.PictureBox();
+            this.lblTarget = new System.Windows.Forms.Label();
+            this.lblSource = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCharacter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTarget)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ilCharacter
+            // 
+            this.ilCharacter.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilCharacter.ImageStream")));
+            this.ilCharacter.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilCharacter.Images.SetKeyName(0, "down1.png");
+            this.ilCharacter.Images.SetKeyName(1, "down2.png");
+            this.ilCharacter.Images.SetKeyName(2, "left1.png");
+            this.ilCharacter.Images.SetKeyName(3, "left2.png");
+            this.ilCharacter.Images.SetKeyName(4, "right1.png");
+            this.ilCharacter.Images.SetKeyName(5, "right2.png");
+            this.ilCharacter.Images.SetKeyName(6, "up1.png");
+            this.ilCharacter.Images.SetKeyName(7, "up2.png");
+            this.ilCharacter.Images.SetKeyName(8, "idle.png");
+            // 
+            // pbCharacter
+            // 
+            this.pbCharacter.Location = new System.Drawing.Point(118, 54);
+            this.pbCharacter.Margin = new System.Windows.Forms.Padding(2);
+            this.pbCharacter.Name = "pbCharacter";
+            this.pbCharacter.Size = new System.Drawing.Size(48, 48);
+            this.pbCharacter.TabIndex = 0;
+            this.pbCharacter.TabStop = false;
+            // 
+            // timerCharacter
+            // 
+            this.timerCharacter.Interval = 50;
+            this.timerCharacter.Tick += new System.EventHandler(this.timerCharacter_Tick);
+            // 
+            // pbSource
+            // 
+            this.pbSource.Image = ((System.Drawing.Image)(resources.GetObject("pbSource.Image")));
+            this.pbSource.Location = new System.Drawing.Point(9, 28);
+            this.pbSource.Margin = new System.Windows.Forms.Padding(2);
+            this.pbSource.Name = "pbSource";
+            this.pbSource.Size = new System.Drawing.Size(105, 105);
+            this.pbSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbSource.TabIndex = 1;
+            this.pbSource.TabStop = false;
+            // 
+            // pbTarget
+            // 
+            this.pbTarget.Image = ((System.Drawing.Image)(resources.GetObject("pbTarget.Image")));
+            this.pbTarget.Location = new System.Drawing.Point(490, 245);
+            this.pbTarget.Margin = new System.Windows.Forms.Padding(2);
+            this.pbTarget.Name = "pbTarget";
+            this.pbTarget.Size = new System.Drawing.Size(105, 105);
+            this.pbTarget.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbTarget.TabIndex = 2;
+            this.pbTarget.TabStop = false;
+            // 
+            // lblTarget
+            // 
+            this.lblTarget.AutoSize = true;
+            this.lblTarget.Location = new System.Drawing.Point(528, 352);
+            this.lblTarget.Name = "lblTarget";
+            this.lblTarget.Size = new System.Drawing.Size(29, 13);
+            this.lblTarget.TabIndex = 3;
+            this.lblTarget.Text = "label";
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(43, 135);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(29, 13);
+            this.lblSource.TabIndex = 4;
+            this.lblSource.Text = "label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(600, 384);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.lblTarget);
+            this.Controls.Add(this.pbTarget);
+            this.Controls.Add(this.pbSource);
+            this.Controls.Add(this.pbCharacter);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCharacter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTarget)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList ilCharacter;
+        private System.Windows.Forms.PictureBox pbCharacter;
+        private System.Windows.Forms.Timer timerCharacter;
+        private System.Windows.Forms.PictureBox pbSource;
+        private System.Windows.Forms.PictureBox pbTarget;
+        private System.Windows.Forms.Label lblTarget;
+        private System.Windows.Forms.Label lblSource;
+    }
+}
+
---
+++ Sezgin/Homeworks/Homework3/Homework3/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework3
+{
+    public partial class Form1 : Form
+    {
+        private const int AppleCount = 2;
+        private int bringedAppleCount;
+        private int count;
+        private Character character;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            count = 0;
+            bringedAppleCount = 0;
+            lblSource.Text = (AppleCount - 1) + " left";
+            Point source = new Point(pbSource.Location.X + pbSource.Width, pbSource.Location.Y);
+            Point target = new Point(pbTarget.Location.X, pbTarget.Location.Y - pbCharacter.Height);
+            character = new Character(source, target, this);
+            timerCharacter.Start();
+        }
+
+        private void timerCharacter_Tick(object sender, EventArgs e)
+        {
+            count++;
+            character.Refresh();
+            pbCharacter.Location = character.getCurrentLocation();
+            int[] images = character.ImageIndexes();
+            if (count % 2 == 0)
+                pbCharacter.Image = ilCharacter.Images[images[0]];
+            else
+                pbCharacter.Image = ilCharacter.Images[images[1]];
+        }
+
+        public void TriggerTargetLbl()
+        {
+            bringedAppleCount++;
+            if (bringedAppleCount >= AppleCount)
+            {
+                timerCharacter.Stop();
+                pbCharacter.Image = ilCharacter.Images[8];
+            }
+            lblTarget.Text = bringedAppleCount + " bringed.";
+        }
+
+        public void TriggerSourceLbl()
+        {
+            lblSource.Text = (AppleCount - bringedAppleCount - 1) + " left";
+        }
+    }
+
+    class Character
+    {
+        private Form1 MainForm;
+        private bool RightControl, DownControl;
+        private Point CurrentLocation;
+        private Point SourceLocation, TargetLocation;
+        private Direction direction;
+
+        private enum Direction
+        {
+            Right,
+            Down,
+            Up,
+            Left
+        }
+
+        public Character(Point sourceLocation, Point targetLocation, Form1 form)
+        {
+            MainForm = form;
+            RightControl = true;
+            DownControl = true;
+            SourceLocation = sourceLocation;
+            TargetLocation = targetLocation;
+            CurrentLocation = new Point(sourceLocation.X + 5, sourceLocation.Y + 10);
+            direction = Direction.Right;
+        }
+
+        private void Move()
+        {
+            if (RightControl && DownControl) //Go Right
+                CurrentLocation.X += 5;
+            else if (!RightControl && DownControl) //Go Down
+                CurrentLocation.Y += 5;
+            else if (RightControl && !DownControl) //Go Up
+                CurrentLocation.Y -= 5;
+            else //Go Left
+                CurrentLocation.X -= 5;
+        }
+
+        public void Refresh()
+        {
+            switch (direction)
+            {
+                case Direction.Right:
+                    goRight();
+                    break;
+                case Direction.Down:
+                    goDown();
+                    break;
+                case Direction.Up:
+                    goUp();
+                    break;
+                case Direction.Left:
+                    goLeft();
+                    break;
+            }
+
+            Move();
+        }
+
+        private void goRight()
+        {
+            if (CurrentLocation.X >= TargetLocation.X + 5)
+            {
+                CurrentLocation.X = TargetLocation.X + 5;
+                RightControl = false;
+                DownControl = true;
+                direction = Direction.Down;
+            }
+        }
+
+        private void goDown()
+        {
+            if (CurrentLocation.Y >= TargetLocation.Y - 5)
+            {
+                CurrentLocation.Y = TargetLocation.Y - 5;
+                RightControl = true;
+                DownControl = false;
+                MainForm.TriggerTargetLbl();
+                direction = Direction.Up;
+            }
+        }
+
+        private void goUp()
+        {
+            if (CurrentLocation.Y <= SourceLocation.Y + 5)
+            {
+                CurrentLocation.Y = SourceLocation.Y + 5;
+                RightControl = false;
+                DownControl = false;
+                direction = Direction.Left;
+            }
+        }
+
+        private void goLeft()
+        {
+            if (CurrentLocation.X <= SourceLocation.X + 5)
+            {
+                CurrentLocation.X = SourceLocation.X + 5;
+                RightControl = true;
+                DownControl = true;
+                MainForm.TriggerSourceLbl();
+                direction = Direction.Right;
+            }
+        }
+
+        public int[] ImageIndexes()
+        {
+            switch (direction)
+            {
+                case Direction.Right:
+                    return new int[] { 4, 5 };
+                case Direction.Down:
+                    return new int[] { 0, 1 };
+                case Direction.Left:
+                    return new int[] { 2, 3 };
+                case Direction.Up:
+                    return new int[] { 6, 7 };
+                default:
+                    return new int[] { 4, 5 };
+            }
+        }
+
+        public Point getCurrentLocation()
+        {
+            return CurrentLocation;
+        }
+    }
+}
