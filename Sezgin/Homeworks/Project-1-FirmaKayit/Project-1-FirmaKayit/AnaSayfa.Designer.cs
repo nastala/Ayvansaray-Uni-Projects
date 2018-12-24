f4f7b6ba865f1ae5fa99af1255ac5d3b3fed4dd7
nastala <sezginyagli@gmail.com>
2018-12-24 11:31:14 +0300

    ...

---
+++ Sezgin/Homeworks/Project-1-FirmaKayit/Project-1-FirmaKayit/AnaSayfa.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Project_1_FirmaKayit
+{
+    partial class AnaSayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.btnForm3 = new System.Windows.Forms.Button();
+            this.btnForm2 = new System.Windows.Forms.Button();
+            this.btnBilgiGiris = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pnl3 = new System.Windows.Forms.Panel();
+            this.pnl2 = new System.Windows.Forms.Panel();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.btnKapat);
+            this.panel1.Controls.Add(this.btnForm3);
+            this.panel1.Controls.Add(this.btnForm2);
+            this.panel1.Controls.Add(this.btnBilgiGiris);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKapat.Location = new System.Drawing.Point(669, 24);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(101, 50);
+            this.btnKapat.TabIndex = 0;
+            this.btnKapat.Text = "Kapat";
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // btnForm3
+            // 
+            this.btnForm3.Location = new System.Drawing.Point(242, 23);
+            this.btnForm3.Name = "btnForm3";
+            this.btnForm3.Size = new System.Drawing.Size(101, 50);
+            this.btnForm3.TabIndex = 0;
+            this.btnForm3.Text = "Form 3";
+            this.btnForm3.UseVisualStyleBackColor = true;
+            this.btnForm3.Click += new System.EventHandler(this.btnForm3_Click);
+            // 
+            // btnForm2
+            // 
+            this.btnForm2.Location = new System.Drawing.Point(135, 23);
+            this.btnForm2.Name = "btnForm2";
+            this.btnForm2.Size = new System.Drawing.Size(101, 50);
+            this.btnForm2.TabIndex = 0;
+            this.btnForm2.Text = "Form 2";
+            this.btnForm2.UseVisualStyleBackColor = true;
+            this.btnForm2.Click += new System.EventHandler(this.btnForm2_Click);
+            // 
+            // btnBilgiGiris
+            // 
+            this.btnBilgiGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBilgiGiris.Location = new System.Drawing.Point(28, 23);
+            this.btnBilgiGiris.Name = "btnBilgiGiris";
+            this.btnBilgiGiris.Size = new System.Drawing.Size(101, 50);
+            this.btnBilgiGiris.TabIndex = 0;
+            this.btnBilgiGiris.Text = "Bilgi Giriş";
+            this.btnBilgiGiris.UseVisualStyleBackColor = true;
+            this.btnBilgiGiris.Click += new System.EventHandler(this.btnBilgiGiris_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.pnl3);
+            this.groupBox1.Controls.Add(this.pnl2);
+            this.groupBox1.Controls.Add(this.pnl1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 350);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // pnl3
+            // 
+            this.pnl3.BackColor = System.Drawing.Color.Aqua;
+            this.pnl3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl3.Location = new System.Drawing.Point(267, 16);
+            this.pnl3.Name = "pnl3";
+            this.pnl3.Size = new System.Drawing.Size(132, 331);
+            this.pnl3.TabIndex = 0;
+            this.pnl3.Visible = false;
+            // 
+            // pnl2
+            // 
+            this.pnl2.BackColor = System.Drawing.Color.Crimson;
+            this.pnl2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl2.Location = new System.Drawing.Point(135, 16);
+            this.pnl2.Name = "pnl2";
+            this.pnl2.Size = new System.Drawing.Size(132, 331);
+            this.pnl2.TabIndex = 0;
+            this.pnl2.Visible = false;
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnl1.Location = new System.Drawing.Point(3, 16);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(132, 331);
+            this.pnl1.TabIndex = 0;
+            // 
+            // AnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "AnaSayfa";
+            this.Text = "AnaSayfa";
+            this.Load += new System.EventHandler(this.AnaSayfa_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.Button btnForm3;
+        private System.Windows.Forms.Button btnForm2;
+        private System.Windows.Forms.Button btnBilgiGiris;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel pnl3;
+        private System.Windows.Forms.Panel pnl2;
+        private System.Windows.Forms.Panel pnl1;
+    }
+}
---
+++ Sezgin/Homeworks/Project-1-FirmaKayit/Project-1-FirmaKayit/AnaSayfa.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1_FirmaKayit
+{
+    public partial class AnaSayfa : Form
+    {
+        enum Paneller
+        {
+            Panel1,
+            Panel2,
+            Panel3
+        }
+
+        public AnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void AnaSayfa_Load(object sender, EventArgs e)
+        {
+            setVisibilityOfPanels(Paneller.Panel1);
+        }
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBilgiGiris_Click(object sender, EventArgs e)
+        {
+            setVisibilityOfPanels(Paneller.Panel1);
+        }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            setVisibilityOfPanels(Paneller.Panel2);
+        }
+
+        private void btnForm3_Click(object sender, EventArgs e)
+        {
+            setVisibilityOfPanels(Paneller.Panel3);
+        }
+
+        private void setVisibilityOfPanels(AnaSayfa.Paneller paneller)
+        {
+            pnl1.Visible = false;
+            pnl2.Visible = false;
+            pnl3.Visible = false;
+
+            switch (paneller)
+            {
+                case Paneller.Panel1:
+                    pnl1.Visible = true;
+                    groupBox1.Text = "Bilgi Giriş Ekranı";
+                    groupBox1.ForeColor = Color.Navy;
+                    break;
+                case Paneller.Panel2:
+                    pnl2.Visible = true;
+                    groupBox1.Text = "Form 2";
+                    groupBox1.ForeColor = Color.Crimson;
+                    break;
+                case Paneller.Panel3:
+                    pnl3.Visible = true;
+                    groupBox1.Text = "Form 3";
+                    groupBox1.ForeColor = Color.Aqua;
+                    break;
+            }
+        }
+    }
+}
