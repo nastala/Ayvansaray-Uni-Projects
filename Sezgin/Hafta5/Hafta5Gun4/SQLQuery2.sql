51cc837dcd2f67390a03bec4d5d334e759cc8204
nastala <sezginyagli@gmail.com>
2018-12-27 14:19:00 +0300

    ...

---
+++ Sezgin/Hafta5/Hafta5Gun4/SQLQuery2.sql
@@ -0,0 +1,20 @@
+--Soru 1
+SELECT Products.ProductName, Categories.CategoryName FROM Products
+INNER JOIN Categories ON Categories.CategoryID = Products.CategoryID
+
+--Soru2
+SELECT Products.ProductName, Customers.CompanyName FROM Customers
+INNER JOIN Orders ON Orders.CustomerID = Customers.CustomerID
+INNER JOIN [Order Details] ON [Order Details].OrderID = Orders.OrderID
+INNER JOIN Products ON Products.ProductID = [Order Details].ProductID
+
+--Soru 3
+SELECT Employees.FirstName + ' ' + Employees.LastName as Name, q1.TotalSales, Orders.OrderDate FROM Employees
+INNER JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
+INNER JOIN (SELECT [Order Details].OrderID, COUNT([Order Details].OrderID) as TotalSales FROM [Order Details] GROUP BY [Order Details].OrderID) q1 ON q1.OrderID = Orders.OrderID
+
+--Soru 4
+SELECT Products.ProductName, Categories.CategoryName FROM Products
+RIGHT JOIN Categories ON Categories.CategoryID = Products.ProductID
+
+--Soru 5
