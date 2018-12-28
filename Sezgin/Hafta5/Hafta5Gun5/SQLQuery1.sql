edda943cee0e07c22eb5c6188f9d72dd977c155b
nastala <sezginyagli@gmail.com>
2018-12-28 10:42:23 +0300

    ...

--- Sezgin/Hafta5/Hafta5Gun5/SQLQuery1.sql
+++ Sezgin/Hafta5/Hafta5Gun5/SQLQuery1.sql
@@ -15,4 +15,8 @@ SELECT booksales.year, SUM(booksales.sales) as total FROM booksales GROUP BY boo
 
 SELECT country, year, genre, SUM(sales) FROM booksales GROUP BY country, year, genre WITH ROLLUP
 
-
+SELECT s.CompanyName, Products.ProductName, SUM(Products.UnitsInStock) as 'Units-In-Stock' FROM Products 
+INNER JOIN (SELECT Suppliers.CompanyName, Suppliers.SupplierID FROM Suppliers WHERE Suppliers.Country = 'Germany') s
+ON s.SupplierID = Products.SupplierID
+GROUP BY s.CompanyName, Products.ProductName
+ORDER BY 'Units-In-Stock' DESC
