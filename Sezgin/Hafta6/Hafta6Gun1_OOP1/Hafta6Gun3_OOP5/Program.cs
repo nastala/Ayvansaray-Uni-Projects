dfc289c5a0286ffa0796f140221f955ac5799d2c
nastala <sezginyagli@gmail.com>
2019-01-02 13:08:12 +0300

    ...

---
+++ Sezgin/Hafta6/Hafta6Gun1_OOP1/Hafta6Gun3_OOP5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hafta6Gun3_OOP5
+{
+    class Program
+    {
+        // Primitive Değişkenler
+        static void Main(string[] args)
+        {
+            ConsoleColor onRenk = ConsoleColor.White;
+            ConsoleColor arkaRenk = ConsoleColor.DarkYellow;
+            Console.BackgroundColor = arkaRenk;
+            Console.ForegroundColor = onRenk;
+
+            int x = 100;
+            decimal y = 100;
+            bool deger = true;
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sezgin/Hafta6/Hafta6Gun1_OOP1/Hafta6Gun3_OOP6/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta6Gun3_OOP6
+{
+    public partial class Form1 : Form
+    {
+        // İlkel nesne tabanlı programlama 
+
+        int[] id = { 1, 2, 3 };
+        string[] ad = { "Ali", "Veli", "Ahmet" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int girilenId = Int32.Parse(textBox1.Text);
+            switch (girilenId)
+            {
+                case 1:
+                    textBox2.Text = ad[0];
+                    break;
+                case 2:
+                    textBox2.Text = ad[1];
+                    break;
+                case 3:
+                    textBox2.Text = ad[2];
+                    break;
+            }
+        }
+    }
+}
