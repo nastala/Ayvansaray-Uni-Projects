8602ebcb7d7b3c8b73749b4e442c8797d6c88bea
nastala <sezginyagli@gmail.com>
2019-01-02 13:29:20 +0300

    ...

---
+++ Sezgin/Hafta6/Hafta6Gun1_OOP1/Hafta6Gun3_OOP7/Classlar/Ogrenci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta6Gun3_OOP7.Classlar
+{
+    class Ogrenci
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Maas { get; set; }
+    }
+}
