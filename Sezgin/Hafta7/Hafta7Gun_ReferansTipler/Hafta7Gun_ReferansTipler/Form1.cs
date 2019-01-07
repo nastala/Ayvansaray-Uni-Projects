f11b5332c334354cb3839923ab6491be7406ee2a
nastala <sezginyagli@gmail.com>
2019-01-07 11:09:20 +0300

    ...

---
+++ Sezgin/Hafta7/Hafta7Gun_ReferansTipler/Hafta7Gun_ReferansTipler/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta7Gun_ReferansTipler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button b1 = new Button();
+            b1.Text = "Button 1";
+
+            Button b2 = b1;
+            b2.Text = "Button 2";
+
+            MessageBox.Show(string.Format("B1: {0}, B2: {1}", b1.Text, b2.Text));
+        }
+    }
+}
