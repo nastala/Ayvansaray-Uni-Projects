d4ce36643579c6830b8315d99122cbeac3a5a313
Ömer Can Danacıoğlu <omercd@hotmail.com.tr>
2019-01-10 11:32:45 +0300

    Add files via upload

---
+++ Ömer/DelegateTutorialSimplified/DelegateTutorial/Classes/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTutorial.Classes
+{
+    internal class Player
+    {
+        public int HP { get; private set; }
+        public string Name { get; set; }
+        public event Action<Player> PlayerDied;
+
+
+        public Player(Action<Player> deathEvent, int hp, string playerName)
+        {
+            PlayerDied += deathEvent;
+            Name = playerName;
+            HP = hp;
+        }
+
+        private void CheckDeathStatus()
+        {
+            if (HP <= 0)
+            {
+                PlayerDied?.Invoke(this);
+            }
+        }
+        public void IncreaseHealth(int amount)
+        {
+            HP += amount;
+            Console.WriteLine(Name + " : Increased HP by " + amount + "\n" + this);
+            CheckDeathStatus();
+        }
+        public override string ToString()
+        {
+            return Name + " : " + HP +" "+ nameof(HP);
+        }
+    }
+}
---
+++ Ömer/DelegateTutorialSimplified/DelegateTutorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelegateTutorial.Classes;
+
+namespace DelegateTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(PlayerDied, 100, "Player1");
+            Player player2 = new Player(PlayerDied, 100, "Player2");
+            player1.IncreaseHealth(-30);
+            player2.IncreaseHealth(-100);
+            player1.IncreaseHealth(-80);
+            Console.ReadKey();
+        }
+
+        private static void PlayerDied(Player player)
+        {
+            Console.WriteLine(player.Name + $" has died");
+        }
+    }
+}
