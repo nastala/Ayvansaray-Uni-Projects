f70d34bb29f71c59ae9d8b9c4825aa65ab137b87
nastala <sezginyagli@gmail.com>
2019-01-21 14:22:08 +0300

    Hafta9Gun1 ADO_Giris

---
+++ Sezgin/Hafta9/Hafta9Gun1/ADO_Giris/3_Disconnected_Mimari_1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace _3_Disconnected_Mimari_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbProductName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCategories = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.nudUnitPrice = new System.Windows.Forms.NumericUpDown();
+            this.nudUnitsInStock = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitsInStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(279, 24);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(55, 29);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product Name";
+            // 
+            // tbProductName
+            // 
+            this.tbProductName.Location = new System.Drawing.Point(6, 33);
+            this.tbProductName.Name = "tbProductName";
+            this.tbProductName.Size = new System.Drawing.Size(72, 20);
+            this.tbProductName.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nudUnitsInStock);
+            this.groupBox1.Controls.Add(this.nudUnitPrice);
+            this.groupBox1.Controls.Add(this.btnCategories);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbProductName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(419, 72);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnCategories
+            // 
+            this.btnCategories.Location = new System.Drawing.Point(340, 24);
+            this.btnCategories.Name = "btnCategories";
+            this.btnCategories.Size = new System.Drawing.Size(73, 29);
+            this.btnCategories.TabIndex = 7;
+            this.btnCategories.Text = "Categories";
+            this.btnCategories.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Units In Stock";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Unit Price";
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(6, 92);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.Size = new System.Drawing.Size(419, 190);
+            this.dgvProducts.TabIndex = 4;
+            this.dgvProducts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProducts_CellClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvProducts);
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(432, 293);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // nudUnitPrice
+            // 
+            this.nudUnitPrice.Location = new System.Drawing.Point(110, 33);
+            this.nudUnitPrice.Name = "nudUnitPrice";
+            this.nudUnitPrice.Size = new System.Drawing.Size(71, 20);
+            this.nudUnitPrice.TabIndex = 8;
+            // 
+            // nudUnitsInStock
+            // 
+            this.nudUnitsInStock.Location = new System.Drawing.Point(195, 33);
+            this.nudUnitsInStock.Name = "nudUnitsInStock";
+            this.nudUnitsInStock.Size = new System.Drawing.Size(71, 20);
+            this.nudUnitsInStock.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 308);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitsInStock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbProductName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCategories;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown nudUnitsInStock;
+        private System.Windows.Forms.NumericUpDown nudUnitPrice;
+    }
+}
+
---
+++ Sezgin/Hafta9/Hafta9Gun1/ADO_Giris/3_Disconnected_Mimari_1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_Disconnected_Mimari_1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection _conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _conn = new SqlConnection("Data Source=.;Initial Catalog=Northwind;Integrated Security=True");
+            fillDgvProducts();
+        }
+
+        private void fillDgvProducts()
+        {
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            dataAdapter.SelectCommand = new SqlCommand("SELECT * FROM Products", _conn);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            dgvProducts.DataSource = dataTable;
+            dgvProducts.Columns["CategoryID"].Visible = false;
+            dgvProducts.Columns["ProductID"].Visible = false;
+            dgvProducts.Columns["SupplierID"].Visible = false;
+        }
+
+        private void dgvProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                dgvProducts.Rows[e.RowIndex].Selected = true;
+                tbProductName.Text = dgvProducts.Rows[e.RowIndex].Cells["ProductName"].Value.ToString();
+                nudUnitPrice.Value = (decimal)dgvProducts.Rows[e.RowIndex].Cells["UnitPrice"].Value;
+                nudUnitsInStock.Text = dgvProducts.Rows[e.RowIndex].Cells["UnitsInStock"].Value.ToString();
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show("Error message: " + exc.Message);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string productName = tbProductName.Text;
+            decimal unitPrice = nudUnitPrice.Value;
+            int unitsInStock = (int)nudUnitsInStock.Value;
+            if(string.IsNullOrWhiteSpace(productName) || unitPrice < 0 || unitsInStock < 0)
+            {
+                MessageBox.Show("You entered wrong or empty values");
+                return;
+            }
+
+            string query = "INSERT INTO Products(ProductName, UnitPrice, UnitsInStock) VALUES(@productName, @unitPrice, @unitsInStock)";
+            try
+            {
+                SqlCommand command = new SqlCommand(query, _conn);
+                command.Parameters.Add(new SqlParameter("productName", productName));
+                command.Parameters.Add(new SqlParameter("unitPrice", unitPrice));
+                command.Parameters.Add(new SqlParameter("unitsInStock", unitsInStock));
+                _conn.Open();
+                command.ExecuteNonQuery();
+                fillDgvProducts();
+            }
+            finally
+            {
+                _conn.Close();
+            }
+        }
+    }
+}
