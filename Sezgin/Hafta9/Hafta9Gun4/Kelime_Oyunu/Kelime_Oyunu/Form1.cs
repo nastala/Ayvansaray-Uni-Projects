05fd9e8174f6f5135af7bb9535b500931fce264e
nastala <sezginyagli@gmail.com>
2019-01-24 09:09:35 +0300

    ...

---
+++ Sezgin/Hafta9/Hafta9Gun4/Kelime_Oyunu/Kelime_Oyunu/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kelime_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection _conn;
+        private List<string> _productNames;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _conn = new SqlConnection("Data Source=.;Initial Catalog=Northwind;Integrated Security=True");
+            _productNames = new List<string>();
+            FillProductNames();
+        }
+
+        private void FillProductNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
