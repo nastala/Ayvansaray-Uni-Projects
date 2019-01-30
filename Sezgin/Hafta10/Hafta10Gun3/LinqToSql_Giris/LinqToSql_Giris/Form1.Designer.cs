5aae78a13b38c7302f87801bf24883ed749a3186
nastala <sezginyagli@gmail.com>
2019-01-30 13:57:19 +0300

    ...

---
+++ Sezgin/Hafta10/Hafta10Gun3/LinqToSql_Giris/LinqToSql_Giris/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace LinqToSql_Giris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbSupplier = new System.Windows.Forms.ComboBox();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.nudUnitsInStock = new System.Windows.Forms.NumericUpDown();
+            this.nudUnitPrice = new System.Windows.Forms.NumericUpDown();
+            this.tbProductName = new System.Windows.Forms.TextBox();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitsInStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(713, 19);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 17;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(578, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Supplier";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(430, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Stock";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Product Name";
+            // 
+            // cbSupplier
+            // 
+            this.cbSupplier.FormattingEnabled = true;
+            this.cbSupplier.Location = new System.Drawing.Point(581, 22);
+            this.cbSupplier.Name = "cbSupplier";
+            this.cbSupplier.Size = new System.Drawing.Size(121, 21);
+            this.cbSupplier.TabIndex = 10;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(433, 22);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cbCategory.TabIndex = 11;
+            // 
+            // nudUnitsInStock
+            // 
+            this.nudUnitsInStock.Location = new System.Drawing.Point(286, 22);
+            this.nudUnitsInStock.Name = "nudUnitsInStock";
+            this.nudUnitsInStock.Size = new System.Drawing.Size(120, 20);
+            this.nudUnitsInStock.TabIndex = 8;
+            // 
+            // nudUnitPrice
+            // 
+            this.nudUnitPrice.Location = new System.Drawing.Point(139, 22);
+            this.nudUnitPrice.Name = "nudUnitPrice";
+            this.nudUnitPrice.Size = new System.Drawing.Size(120, 20);
+            this.nudUnitPrice.TabIndex = 9;
+            // 
+            // tbProductName
+            // 
+            this.tbProductName.Location = new System.Drawing.Point(12, 22);
+            this.tbProductName.Name = "tbProductName";
+            this.tbProductName.Size = new System.Drawing.Size(100, 20);
+            this.tbProductName.TabIndex = 7;
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 49);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.Size = new System.Drawing.Size(938, 381);
+            this.dgvMain.TabIndex = 6;
+            this.dgvMain.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMain_CellClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(794, 19);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(875, 19);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 443);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Search by Product Name";
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(144, 440);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(100, 20);
+            this.tbSearch.TabIndex = 21;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 465);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSupplier);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.nudUnitsInStock);
+            this.Controls.Add(this.nudUnitPrice);
+            this.Controls.Add(this.tbProductName);
+            this.Controls.Add(this.dgvMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitsInStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudUnitPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbSupplier;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.NumericUpDown nudUnitsInStock;
+        private System.Windows.Forms.NumericUpDown nudUnitPrice;
+        private System.Windows.Forms.TextBox tbProductName;
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbSearch;
+    }
+}
+
---
+++ Sezgin/Hafta10/Hafta10Gun3/LinqToSql_Giris/LinqToSql_Giris/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToSql_Giris
+{
+    public partial class Form1 : Form
+    {
+        private NorthwindDataContext _dataContext = new NorthwindDataContext();
+        private List<Product> _products;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //dgvMain.DataSource = _dataContext.Products;
+
+            cbCategory.DisplayMember = "CategoryName";
+            cbCategory.ValueMember = "CategoryID";
+            cbCategory.DataSource = _dataContext.Categories;
+
+            cbSupplier.DisplayMember = "CompanyName";
+            cbSupplier.ValueMember = "SupplierID";
+            cbSupplier.DataSource = _dataContext.Suppliers;
+
+            dgvMain.DataSource = _dataContext.Products;
+            //dgvMain.DataSource = GetDataSource();
+        }
+
+        private IQueryable GetDataSource()
+        {
+            return from product in _dataContext.Products
+                   join category in _dataContext.Categories on product.CategoryID equals category.CategoryID
+                   select new
+                   {
+                       product.ProductID,
+                       product.ProductName,
+                       product.UnitPrice,
+                       product.UnitsInStock,
+                       product.CategoryID,
+                       product.SupplierID,
+                       product.QuantityPerUnit,
+                       category.CategoryName
+                   };
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Product product = new Product();
+            product.ProductName = tbProductName.Text;
+            product.UnitPrice = nudUnitPrice.Value;
+            product.UnitsInStock = Convert.ToInt16(nudUnitsInStock.Value);
+            product.CategoryID = (int)cbCategory.SelectedValue;
+            product.SupplierID = (int)cbSupplier.SelectedValue;
+
+            _dataContext.Products.InsertOnSubmit(product);
+            MessageBox.Show($"ProductID before SubmitChanges = {product.ProductID}");
+            _dataContext.SubmitChanges();
+            MessageBox.Show($"ProductID after SubmitChanges = {product.ProductID}");
+
+            //dgvMain.DataSource = GetDataSource(); //63. ve 64. satırların işlevi aynı. İki şekilde de DataGridView'u güncelleyebiliriz. 
+            //Bir diğer çözüm ise DataContext'i her seferinde tekrar initialize etmektir. _dataContext = new NorthwindDataContext();
+            dgvMain.DataSource = _dataContext.Products.GetNewBindingList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvMain.CurrentRow == null)
+                return;
+
+            int productID = (int)dgvMain.CurrentRow.Cells["ProductID"].Value;
+            Product product = _dataContext.Products.SingleOrDefault(p => p.ProductID == productID);
+
+            _dataContext.Products.DeleteOnSubmit(product);
+            _dataContext.SubmitChanges();
+
+            //dgvMain.DataSource = GetDataSource();
+            dgvMain.DataSource = _dataContext.Products.GetNewBindingList();
+        }
+
+        private void dgvMain_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dgvMain.CurrentRow;
+
+            tbProductName.Text = row.Cells["ProductName"].Value.ToString();
+            nudUnitPrice.Value = Convert.ToDecimal(row.Cells["UnitPrice"].Value);
+            nudUnitsInStock.Value = Convert.ToInt16(row.Cells["UnitsInStock"].Value);
+            cbCategory.SelectedValue = (int)row.Cells["CategoryID"].Value;
+            cbSupplier.SelectedValue = (int)row.Cells["SupplierID"].Value;
+
+            dgvMain.Rows[e.RowIndex].Selected = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (dgvMain.CurrentRow == null)
+                return;
+
+            Product product = (Product)dgvMain.CurrentRow.DataBoundItem;
+            product.ProductID = Convert.ToInt32(dgvMain.CurrentRow.Cells["ProductID"].Value);
+            product.ProductName = tbProductName.Text;
+            product.UnitPrice = nudUnitPrice.Value;
+            product.UnitsInStock = Convert.ToInt16(nudUnitsInStock.Value);
+            product.CategoryID = Convert.ToInt32(cbCategory.SelectedValue);
+            product.SupplierID = Convert.ToInt32(cbSupplier.SelectedValue);
+
+            _dataContext.SubmitChanges();
+            //dgvMain.DataSource = GetDataSource();
+            dgvMain.DataSource = _dataContext.Products.GetNewBindingList();
+        }
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            dgvMain.DataSource = _dataContext.Products.Where(product => product.ProductName.Contains(tbSearch.Text));
+        }
+    }
+}
