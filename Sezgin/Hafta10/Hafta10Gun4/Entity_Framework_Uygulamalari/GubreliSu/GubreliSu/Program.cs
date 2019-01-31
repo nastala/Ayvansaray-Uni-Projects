0e74665b3e8fe809b98b231a7437c273ecae021c
nastala <sezginyagli@gmail.com>
2019-01-31 11:56:20 +0300

    GubreliSu Eklendi

---
+++ Sezgin/Hafta10/Hafta10Gun4/Entity_Framework_Uygulamalari/GubreliSu/GubreliSu/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GubreliSu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    cBitki b1 = new cBitki();
+            //    b1.Beslenme = "gübre";
+            //    cBitki b2 = b1;
+            //    b2.Beslenme = "su";
+            //    Console.WriteLine(b1.Beslenme);
+
+            var bitkiler = new cBitki[] 
+            {
+                new cBitki
+                {
+                    Beslenme = "gübre"
+                },
+                new cBitki
+                {
+                    Beslenme = "su"
+                }
+            };
+
+            cBitki[] bitkilerClone = (cBitki[])bitkiler.Clone();
+            bitkilerClone[0] = new cBitki(){ Beslenme = "su" };
+
+            Console.WriteLine(bitkiler[0].Beslenme);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sezgin/Hafta10/Hafta10Gun4/Entity_Framework_Uygulamalari/GubreliSu/GubreliSu/cBitki.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GubreliSu
+{
+    class cBitki
+    {
+        public string Beslenme { get; set; }
+    }
+}
