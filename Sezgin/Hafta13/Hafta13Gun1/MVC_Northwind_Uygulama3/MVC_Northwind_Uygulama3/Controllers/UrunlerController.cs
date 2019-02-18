7c2c2279e3a4dad9c2afae5eefbae212a969032c
nastala <sezginyagli@gmail.com>
2019-02-18 14:43:33 +0300

    ...

---
+++ Sezgin/Hafta13/Hafta13Gun1/MVC_Northwind_Uygulama3/MVC_Northwind_Uygulama3/Controllers/UrunlerController.cs
@@ -0,0 +1,23 @@
+﻿using MVC_Northwind_Uygulama3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Northwind_Uygulama3.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        private MyDbContext ctx = new MyDbContext();
+
+        // GET: Urunler
+        public ActionResult Index()
+        {
+            // View methodunun arasına bir değişken vermek o değişkeni model yöntemi ile view'e göndermek demektir. Model yöntemiyle bir anda sadece 
+            //      bir veri gönderilebilir.
+            List<Product> urunler = ctx.Products.ToList();
+            return View(urunler);
+        }
+    }
+}
