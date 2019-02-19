2e2ceaea407e6c67d18111be45cfff4f4b37dfae
nastala <sezginyagli@gmail.com>
2019-02-19 11:16:32 +0300

    ...

--- Sezgin/Hafta13/Hafta13Gun2/MVC_Uygulama1/MVC_Uygulama1/Controllers/UrunController.cs
+++ Sezgin/Hafta13/Hafta13Gun2/MVC_Uygulama1/MVC_Uygulama1/Controllers/UrunController.cs
@@ -12,6 +12,15 @@ namespace MVC_Uygulama1.Controllers
         // GET: Urun
         public ActionResult Index()
         {
+            /*
+                Model yöntemi ile gönderiliyorsa view methodu içerisine yazılır ve modelin tanımlaması cshtml'in
+                    içerisinde yapılır.
+                ViewBag yöntemi ile gönderiliyorsa herhangi bir olmayan dinamik tip tanımlanır ve bu tipin temsil ettiği alana
+                    veriler atanabilir.
+             */
+            IEnumerable<Category> kategoriler = NorthwindModel.Model.Categories.ToList();
+            ViewBag.Kategoriler = kategoriler;
+
             return View(NorthwindModel.Model.Products.ToList());
         }
     }
