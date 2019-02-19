c5f116d3722ceaafb60162063b588b6a9df1d386
nastala <sezginyagli@gmail.com>
2019-02-19 10:02:45 +0300

    ...

---
+++ Sezgin/Hafta13/Hafta13Gun2/MVC_Uygulama1/MVC_Uygulama1/Controllers/UrunController.cs
@@ -0,0 +1,18 @@
+﻿using MVC_Uygulama1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Uygulama1.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        public ActionResult Index()
+        {
+            return View(NorthwindModel.Model.Products.ToList());
+        }
+    }
+}
