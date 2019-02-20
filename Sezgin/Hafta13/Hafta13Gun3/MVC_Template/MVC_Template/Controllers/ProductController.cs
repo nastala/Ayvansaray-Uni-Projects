4f9f63940430c0bf7a14e496fd7178ac43bc4242
nastala <sezginyagli@gmail.com>
2019-02-20 14:50:15 +0300

    ..

---
+++ Sezgin/Hafta13/Hafta13Gun3/MVC_Template/MVC_Template/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using MVC_Template.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Template.Controllers
+{
+    public class ProductController : Controller
+    {
+        private NorthwindModel _model = new NorthwindModel();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_model.Products.ToList());
+        }
+
+        public ActionResult Details(int productID)
+        {
+            Product product = _model.Products.Find(productID);
+            ViewBag.SupplierID = new SelectList(_model.Suppliers, "SupplierID", "CompanyName");
+            return View(product);
+        }
+    }
+}
