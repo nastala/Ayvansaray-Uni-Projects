63e1befc5864e371e906a97872cd88e3379b7750
nastala <sezginyagli@gmail.com>
2019-02-21 11:30:59 +0300

    ...

--- Sezgin/Hafta13/Hafta13Gun3/MVC_Template/MVC_Template/Controllers/ProductController.cs
+++ Sezgin/Hafta13/Hafta13Gun3/MVC_Template/MVC_Template/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using MVC_Template.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,11 +18,68 @@ namespace MVC_Template.Controllers
             return View(_model.Products.ToList());
         }
 
-        public ActionResult Details(int productID)
+        public ActionResult Details(int? productID)
         {
+            if (productID == null)
+                return RedirectToAction("Index");
+
             Product product = _model.Products.Find(productID);
-            ViewBag.SupplierID = new SelectList(_model.Suppliers, "SupplierID", "CompanyName");
+
+            if (product == null)
+                return HttpNotFound();
+
+            ViewBag.SupplierID = new SelectList(_model.Suppliers, "SupplierID", "ContactName");
+            ViewBag.CategoryID = new SelectList(_model.Categories, "CategoryID", "CategoryName");
             return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Details([Bind(Include = "ProductID, ProductName, UnitPrice, UnitsInStock, CategoryID, SupplierID, UnitsOnOrder, ReorderLevel, QuantityPerUnit, Discontinued")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _model.Entry(product).State = EntityState.Modified;
+                _model.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Details", new { productID = product.ProductID });
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.SupplierID = new SelectList(_model.Suppliers, "SupplierID", "CompanyName");
+            ViewBag.CategoryID = new SelectList(_model.Categories, "CategoryID", "CategoryName");
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ProductName, UnitPrice, UnitsInStock, CategoryID, SupplierID, UnitsOnOrder, ReorderLevel, QuantityPerUnit, Discontinued")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _model.Products.Add(product);
+                _model.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Create");
+        }
+
+        public ActionResult Delete(int? productID)
+        {
+            if (productID == null)
+                return RedirectToAction("Index");
+
+            Product product = _model.Products.Find(productID);
+
+            if (product == null)
+                return RedirectToAction("Index");
+
+            _model.Products.Remove(product);
+            _model.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
