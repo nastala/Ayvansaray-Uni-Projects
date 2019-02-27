e5ed393b499f892127bc9fa97fd9123a3210b5e6
nastala <sezginyagli@gmail.com>
2019-02-27 11:36:49 +0300

    ...

---
+++ Sezgin/Hafta14/Hafta14Gun2/MVC_Template/MVC_Template/Controllers/KullaniciController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC_Template.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        // Buradaki sayfaları sadece Admin rolüne sahip olan görebilecek.
+
+        // GET: Kullanici
+        public ActionResult Index()
+        {
+            // Veritabanındaki tüm kullanıcıları çekip users isimli değişkenin içerisine atayacak.
+            MembershipUserCollection users = Membership.GetAllUsers();
+            return View(users);
+        }
+    }
+}
