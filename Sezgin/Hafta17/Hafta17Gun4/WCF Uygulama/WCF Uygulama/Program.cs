0b9a262819d29e4deb9f0f8b3b978c91395b5709
nastala <sezginyagli@gmail.com>
2019-03-21 14:15:50 +0300

    ...

---
+++ Sezgin/Hafta17/Hafta17Gun4/WCF Uygulama/WCF Uygulama/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF_Uygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding myBinding = new BasicHttpBinding();
+            EndpointAddress myEndpoint = new EndpointAddress("http://localhost:63503/Service1.svc");
+            ChannelFactory<IService1> myChannelFactory = new ChannelFactory<IService1>(myBinding, myEndpoint);
+            var result = myChannelFactory.CreateChannel().Test();
+            result = myChannelFactory.CreateChannel().GetData(5);
+            Console.ReadKey();
+        }
+    }
+}
